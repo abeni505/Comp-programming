0f13919cf8464a12b8c2f4dfe539c7c1861f4122
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2022-10-05 22:08:48 +0300

    Create Insertion-Sort.py

---
+++ Insertion-Sort.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    # Write your code here
+    key = arr[-1]
+    i = n-1
+    while i > 0 and arr[i-1] > key:  
+        arr[i] = arr[i-1]
+        print(*arr)
+        i -= 1
+    arr[i] = key
+    print(*arr)
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
+
+    
